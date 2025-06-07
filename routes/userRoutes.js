8858990c711254779da17c8b2715251d55f33d71
CraftHub <crafthub@example.com>
2025-06-07 22:18:16 +0530

    done

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,33 +1,33 @@
 import express from "express";
 import userModel from "../models/userModel.js";
-import bycrypt from "bcryptjs";
+import bcrypt from "bcryptjs";
+const userRouter = express.Router();
 import jwt from "jsonwebtoken";
 const SECRET_KEY = "helloworld";
 
-const userRouter = express.Router();
-
 userRouter.post("/register", async (req, res) => {
-  const { name, email, pass } = req.body;
-  const hashedPass = await bycrypt.hash(pass, 10);
-  const result = await userModel.insertOne({
+  const { name, email, pass } = await req.body;
+  // const hashpassword = await bcrypt.hash(pass, 10);
+  const result = await userModel.create({
     name: name,
     email: email,
-    pass: hashedPass,
+    pass: pass,
+    //  pass: hashpassword,
   });
   return res.json(result);
 });
 
 userRouter.post("/login", async (req, res) => {
-  const { email, pass } = req.body;
-  const result = await userModel.findOne({ email: email });
-  if (!result) return res.json({ message: "Invalid" });
-  const isPasswordValid = await bycrypt.compare(pass, result.pass);
-  if (!isPasswordValid) return res.status(404).json({ message: "Invalid password" });
-  const token = jwt.sign({email: result.email, id: result._id}, SECRET_KEY);
+  const { email, pass } = await req.body;
+  const result = await userModel.findOne({ email });
+  if (!result) return res.json({ message: "Invalid user" });
+  const matchPassword = await bcrypt.compare(pass, result.pass);
+  if (!matchPassword) {
+    return res.status(400).json({ message: "Invalid Password" });
+  }
+  const token = jwt.sign({ email: result.email, id: result._id }, SECRET_KEY);
   console.log(result);
   return res.json({ user: result, token: token });
 });
-
-
 
 export default userRouter;
